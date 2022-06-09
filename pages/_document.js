fd85999879dbcac53eced4c4e20400eadc917df4
Rasil07 <rasilgrt@gmail.com>
2022-06-09 08:40:33 +0545

    initialized next with antD and tailwindcss

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/google-font-display */
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Russo+One"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  important: true, // to generate utilities as !important
+  content: [
+    // Add the paths to all of your template files in your
+    "./pages/**/*.{js,ts,tsx,jsx}",
+    "./src/components/**/*.{js,ts,tsx,jsx}",
+    "./layouts/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Russo One", ...defaultTheme.fontFamily.sans],
+        fancy: ["Dancing Script"],
+      },
+    },
+    colors: {
+      ...defaultTheme.colors,
+      primary: "#441151",
+      secondary: "#ccc",
+    },
+  },
+  plugins: [],
+};
